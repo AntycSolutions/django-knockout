1708e3a99dd4a9c88b374c6a9c68df7d21188983
Solanar <and_555@hotmail.com>
2015-05-15 10:50:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-knockout-modeler',
+    name='django-knockout',
     version='0.6.3',
-    packages=['knockout_modeler'],
+    packages=['knockout'],
     install_requires=['simplejson'],
     include_package_data=True,
     license='MIT License',
     description='Super easy ModelViews for knockout.js for your Django models',
     long_description=README,
-    url='https://github.com/Miserlou/django-knockout-modeler',
-    author='Rich Jones',
+    url='https://github.com/AntycSolutions/django-knockout',
+    author='Rich Jones, Airith',
     author_email='rich@openwatch.net',
     classifiers=[
         'Environment :: Web Environment',
@@ -25,8 +25,8 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
