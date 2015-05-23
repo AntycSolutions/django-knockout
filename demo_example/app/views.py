18d5404bb72e5f3beeb3fb2285a06c6587b49206
Solanar <and_555@hotmail.com>
2015-05-23 16:06:04 -0600

    optimize queries (select_related, prefetch_related), add many to many model view

--- demo_example/app/views.py
+++ demo_example/app/views.py
@@ -25,7 +25,23 @@ class Schedule(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Schedule, self).get_context_data(**kwargs)
 
-        schedule = models.Task.objects.all()
+        schedule = models.Task.objects.all().select_related(
+            'day', 'reminder', 'reminder__reminder_type'
+        )
         context['schedule'] = schedule
+
+        return context
+
+
+class Shopping(TemplateView):
+    template_name = "app/shopping.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Shopping, self).get_context_data(**kwargs)
+
+        shopping_lists = models.Shopping.objects.all().prefetch_related(
+            'items'
+        )
+        context['shopping_lists'] = shopping_lists
 
         return context
