18b9e4db4616775d556dfb40213864842f06150b
Solanar <and_555@hotmail.com>
2015-05-24 14:07:24 -0600

    multiple models example, remove redundant class specification in super

--- demo_example/app/views.py
+++ demo_example/app/views.py
@@ -11,7 +11,7 @@ class Persons(TemplateView):
     template_name = "app/persons.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Persons, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
 
         persons = models.Person.objects.all()
         context['persons'] = persons
@@ -23,7 +23,7 @@ class Schedule(TemplateView):
     template_name = "app/schedule.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Schedule, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
 
         schedule = models.Task.objects.all().select_related(
             'day', 'reminder', 'reminder__reminder_type'
@@ -37,7 +37,29 @@ class Shopping(TemplateView):
     template_name = "app/shopping.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Shopping, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+
+        shopping_lists = models.Shopping.objects.all().prefetch_related(
+            'items'
+        )
+        context['shopping_lists'] = shopping_lists
+
+        return context
+
+
+class MultipleModels(TemplateView):
+    template_name = "app/multiple_models.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        persons = models.Person.objects.all()
+        context['persons'] = persons
+
+        schedule = models.Task.objects.all().select_related(
+            'day', 'reminder', 'reminder__reminder_type'
+        )
+        context['schedule'] = schedule
 
         shopping_lists = models.Shopping.objects.all().prefetch_related(
             'items'
