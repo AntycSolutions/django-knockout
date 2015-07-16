5104213904b2b664048f4f6d7df2b9e4234a2b24
Solanar <and_555@hotmail.com>
2015-07-16 17:29:39 -0600

    check for null data, remove bad arg data from add, remove _ prefix from ko variables, indent for better output formatting

--- knockout/templates/knockout/list.js
+++ knockout/templates/knockout/list.js
@@ -1,26 +1,32 @@
-self._{{ model_args }} = ko.observableArray(ko.utils.arrayMap(
-    data.{{ model_args }},
-    function(data) {
-        return new {{ model_name }}(data);
+
+    if (data) {
+        self.{{ model_args }} = ko.observableArray(ko.utils.arrayMap(
+            data.{{ model_args }},
+            function(data) {
+                return new {{ model_name }}(data);
+            }
+        ));
     }
-));
+    else {
+        self.{{ model_args }} = ko.observableArray();
+    }
 
-self.add{{ model_name }} = function(data) {
-    self._{{ model_args }}.push(new {{ model_name }}(data));
-};
+    self.add{{ model_name }} = function() {
+        self.{{ model_args }}.push(new {{ model_name }}());
+    };
 
-self.remove{{ model_name }} = function(data) {
-    self._{{ model_args }}.remove(data);
-};
+    self.remove{{ model_name }} = function(data) {
+        self.{{ model_args }}.remove(data);
+    };
 
-self.sort{{ model_name }}sAsc = function() {
-    self._{{ model_args }}(self._{{ model_args }}().sort(function(a, b) {
-        return a._{{ comparator }}()>b._{{ comparator }}()?-1:a._{{ comparator }}()<b._{{ comparator }}()?1:0;
-     }));
-};
+    self.sort{{ model_name }}sAsc = function() {
+        self.{{ model_args }}(self.{{ model_args }}().sort(function(a, b) {
+            return a.{{ comparator }}()>b.{{ comparator }}()?-1:a.{{ comparator }}()<b.{{ comparator }}()?1:0;
+         }));
+    };
 
-self.sort{{ model_name }}sDesc = function() {
-    self._{{ model_args }}(self._{{ model_args }}().sort(function(a, b) {
-        return a._{{ comparator }}()<b._{{ comparator }}()?-1:a._{{ comparator }}()>b._{{ comparator }}()?1:0;
-    }));
-};
+    self.sort{{ model_name }}sDesc = function() {
+        self.{{ model_args }}(self.{{ model_args }}().sort(function(a, b) {
+            return a.{{ comparator }}()<b.{{ comparator }}()?-1:a.{{ comparator }}()>b.{{ comparator }}()?1:0;
+        }));
+    };
