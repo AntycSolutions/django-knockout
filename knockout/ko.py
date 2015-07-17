775cd483cf5cd25e6dafbc9e334456b5b211af8c
Solanar <and_555@hotmail.com>
2015-07-16 18:32:32 -0600

    create class KnockoutModel which is a recursive (for foreign key/many to manys) representation of django model's fields instead of dicts, better variable names

--- knockout/ko.py
+++ knockout/ko.py
@@ -6,82 +6,57 @@ from django.template.loader import render_to_string
 from django.db.models.fields import related
 
 
-def _get_knockout_fields(model):
-    knockout_fields = model.knockout_fields()
-    fields = []
-    for knockout_field in knockout_fields:
-        fields.append(model._meta.get_field(knockout_field))
+class KnockoutModel():
 
-    return fields
+    def __init__(self, model,
+                 field_name=None,
+                 follow_fks=True, follow_m2ms=True):
+        self.field_name = field_name
+        self.model = model
+        self.follow_fks = follow_fks
+        self.follow_m2ms = follow_m2ms
 
+        self._get_fields(model, follow_fks, follow_m2ms)
 
-def _get_relation_dict(field):
-    model = field.rel.to
-    field_dict = _get_fields(model)
-    fields = field_dict['fields']
-    fk_fields = field_dict['fks']
-    m2m_fields = field_dict['m2ms']
+    def _get_knockout_fields(self, model):
+        knockout_fields = model.knockout_fields()
+        fields = []
+        for knockout_field in knockout_fields:
+            fields.append(model._meta.get_field(knockout_field))
 
-    return {
-        'field_name': field.name, 'model': model,
-        'nested': {
-            'fields': fields,
-            'fks': fk_fields,
-            'm2ms': m2m_fields
-        }
-    }
+        return fields
 
+    def _get_relation(self, field):
+        model = field.rel.to
+        knockout_model = KnockoutModel(model, field_name=field.name)
 
-def _get_fields(model, follow_fks=True, follow_m2ms=True):
-    '''
-    Returns
-        {
-            'm2ms': [
-                {
-                    'field_name': <m2m field name>,
-                    'model': <m2m field rel to>,
-                    'nested': {
-                        'm2ms': [<m2m field rel to's m2m>],
-                        'fks': [<m2m field rel to's fk>],
-                        'fields': [<m2m field rel to's field>]
-                    }
-                }
-            ],
-            'fks': [
-                {
-                    'field_name': <fk field name>,
-                    'model': <fk field rel to>,
-                    'nested': {
-                        'm2ms': [<fk field rel to's m2m>],
-                        'fks': [<fk field rel to's fk>],
-                        'fields': [<fk field rel to's field>]
-                    }
-                }
-            ],
-            'fields': [<field name>],
-        }
-    '''
-    if hasattr(model, 'knockout_fields'):
-        model_fields = _get_knockout_fields(model)
-    else:
-        model_fields = model._meta.get_fields()
+        return knockout_model
 
-    fields = []
-    fks = []  # Foreign Keys
-    m2ms = []  # Many to Manys
-    for field in model_fields:
-        if follow_fks and isinstance(field, related.ForeignKey):
-            relation_dict = _get_relation_dict(field)
-            fks.append(relation_dict)
-        elif follow_m2ms and isinstance(field, related.ManyToManyField):
-            relation_dict = _get_relation_dict(field)
-            m2ms.append(relation_dict)
-        elif isinstance(field, (related.ManyToOneRel, related.ManyToManyRel)):
-            continue
+    def _get_fields(self, model, follow_fks=True, follow_m2ms=True):
+        if hasattr(model, 'knockout_fields'):
+            model_fields = self._get_knockout_fields(model)
         else:
-            fields.append(field.name)
+            model_fields = model._meta.get_fields()
 
-    return {'fields': fields, 'fks': fks, 'm2ms': m2ms}
+        fields = []
+        fks = []  # Foreign Keys
+        m2ms = []  # Many to Manys
+        for field in model_fields:
+            if follow_fks and isinstance(field, related.ForeignKey):
+                knockout_model = self._get_relation(field)
+                fks.append(knockout_model)
+            elif follow_m2ms and isinstance(field, related.ManyToManyField):
+                knockout_model = self._get_relation(field)
+                m2ms.append(knockout_model)
+            elif isinstance(field, (related.ManyToOneRel,
+                                    related.ManyToManyRel)):
+                continue
+            else:
+                fields.append(field.name)
+
+        self.fields = fields
+        self.fks = fks
+        self.m2ms = m2ms
 
 
 def ko_list(model):
@@ -105,28 +80,28 @@ def ko_list(model):
     return list_string
 
 
-def ko_model(model, all_fields=None):
-    if not all_fields:
-        all_fields = _get_fields(model)
-    fields = all_fields['fields']
-    fk_fields = all_fields['fks']
-    m2m_fields = all_fields['m2ms']
+def ko_model(model, knockout_model=None):
+    if not knockout_model:
+        knockout_model = KnockoutModel(model)
+    fields = knockout_model.fields
+    fk_knockout_models = knockout_model.fks
+    m2m_knockout_models = knockout_model.m2ms
 
     model_name = model.__name__
     model_arg = model_name.lower()
     model_args = model_name.lower() + "s"
 
-    for field_dict in fk_fields:
-        nested = field_dict['nested']
-        fk_model = field_dict['model']
-        field_dict['model_string'] = ko_model(fk_model, nested)
-        field_dict['model_name'] = fk_model.__name__
+    for fk_knockout_model in fk_knockout_models:
+        fk_model = fk_knockout_model.model
+        fk_knockout_model.model_string = ko_model(fk_model,
+                                                  fk_knockout_model)
+        fk_knockout_model.model_name = fk_model.__name__
 
-    for field_dict in m2m_fields:
-        nested = field_dict['nested']
-        m2m_model = field_dict['model']
-        field_dict['model_string'] = ko_model(m2m_model, nested)
-        field_dict['model_list'] = ko_list(m2m_model)
+    for m2m_knockout_model in m2m_knockout_models:
+        m2m_model = m2m_knockout_model.model
+        m2m_knockout_model.model_string = ko_model(m2m_model,
+                                                   m2m_knockout_model)
+        m2m_knockout_model.model_list = ko_list(m2m_model)
 
     model_string = render_to_string(
         "knockout/model.js",
@@ -134,22 +109,22 @@ def ko_model(model, all_fields=None):
          'model_arg': model_arg,
          'model_args': model_args,
          'fields': fields,
-         'fk_fields': fk_fields,
-         'm2m_fields': m2m_fields}
+         'fk_knockout_models': fk_knockout_models,
+         'm2m_knockout_models': m2m_knockout_models}
     )
 
     return model_string
 
 
-def ko_view_model(model, all_fields=None):
-    if not all_fields:
-        all_fields = _get_fields(model)
+def ko_view_model(model, knockout_model=None):
+    if not knockout_model:
+        knockout_model = KnockoutModel(model)
 
     model_name = model.__name__
     view_model_string = model_name + "ViewModel"
 
     model_list_string = ko_list(model)
-    model_string = ko_model(model, all_fields)
+    model_string = ko_model(model, knockout_model)
 
     view_model_string = render_to_string(
         "knockout/view_model.js",
@@ -190,31 +165,32 @@ def _model_data(obj, fields, safe):
     return obj_dict
 
 
-def _model_foreign_key_data(obj, fields, safe):
+def _model_foreign_key_data(obj, knockout_models, safe):
     foreign_key_dict = {}
-    for field in fields:
-        field_name = field['field_name']
+    for knockout_model in knockout_models:
+        field_name = knockout_model.field_name
+
         if hasattr(obj, field_name):
-            field_obj = getattr(obj, field_name)
+            fk_obj = getattr(obj, field_name)
         else:
-            field_obj = None
+            fk_obj = None
 
         all_dict = {}
-        if field['nested']['fields']:
+        if knockout_model.fields:
             foreign_key_obj_dict = _model_data(
-                field_obj, field['nested']['fields'], safe
+                fk_obj, knockout_model.fields, safe
             )
             all_dict.update(foreign_key_obj_dict)
 
-        if field['nested']['fks']:
+        if knockout_model.fks:
             nested_foreign_key_obj_dict = _model_foreign_key_data(
-                field_obj, field['nested']['fks'], safe
+                fk_obj, knockout_model.fks, safe
             )
             all_dict.update(nested_foreign_key_obj_dict)
 
-        if field['nested']['m2ms']:
+        if knockout_model.m2ms:
             nested_many_to_many_obj_dict = _model_many_to_many_data(
-                field_obj, field['nested']['m2ms'], safe
+                fk_obj, knockout_model.m2ms, safe
             )
             all_dict.update(nested_many_to_many_obj_dict)
 
@@ -223,30 +199,31 @@ def _model_foreign_key_data(obj, fields, safe):
     return foreign_key_dict
 
 
-def _model_many_to_many_data(obj, fields, safe):
+def _model_many_to_many_data(obj, knockout_models, safe):
     many_to_many_dict = {}
-    for field in fields:
-        field_name = field['field_name']
-        attribute_list = getattr(obj, field_name)
+    for knockout_model in knockout_models:
+        field_name = knockout_model.field_name
+
+        m2m_list = getattr(obj, field_name)
         many_to_many_obj_list = []
-        for attribute in attribute_list.all():
+        for m2m_obj in m2m_list.all():
             all_dict = {}
 
-            if field['nested']['fields']:
+            if knockout_model.fields:
                 many_to_many_obj_dict = _model_data(
-                    attribute, field['nested']['fields'], safe
+                    m2m_obj, knockout_model.fields, safe
                 )
                 all_dict.update(many_to_many_obj_dict)
 
-            if field['nested']['fks']:
+            if knockout_model.fks:
                 nested_foreign_key_obj_dict = _model_foreign_key_data(
-                    field['model'], field['nested']['fks'], safe
+                    m2m_obj, knockout_model.fks, safe
                 )
                 all_dict.update(nested_foreign_key_obj_dict)
 
-            if field['nested']['m2ms']:
+            if knockout_model.m2ms:
                 nested_many_to_many_obj_dict = _model_many_to_many_data(
-                    field['model'], field['nested']['m2ms'], safe
+                    m2m_obj, knockout_model.m2ms, safe
                 )
                 all_dict.update(nested_many_to_many_obj_dict)
 
@@ -257,12 +234,12 @@ def _model_many_to_many_data(obj, fields, safe):
     return many_to_many_dict
 
 
-def ko_data(model, queryset, all_fields=None, safe=False):
-    if not all_fields:
-        all_fields = _get_fields(model)
-    fields = all_fields['fields']
-    fk_fields = all_fields['fks']
-    m2m_fields = all_fields['m2ms']
+def ko_data(model, queryset, knockout_model=None, safe=False):
+    if not knockout_model:
+        knockout_model = KnockoutModel(model)
+    fields = knockout_model.fields
+    fk_knockout_models = knockout_model.fks
+    m2m_knockout_models = knockout_model.m2ms
 
     model_data = []
     for obj in queryset:
@@ -271,10 +248,12 @@ def ko_data(model, queryset, all_fields=None, safe=False):
         obj_dict = _model_data(obj, fields, safe)
         all_dict.update(obj_dict)
 
-        foreign_key_dict = _model_foreign_key_data(obj, fk_fields, safe)
+        foreign_key_dict = _model_foreign_key_data(obj, fk_knockout_models,
+                                                   safe)
         all_dict.update(foreign_key_dict)
 
-        many_to_many_dict = _model_many_to_many_data(obj, m2m_fields, safe)
+        many_to_many_dict = _model_many_to_many_data(obj, m2m_knockout_models,
+                                                     safe)
         all_dict.update(many_to_many_dict)
 
         model_data.append(all_dict)
@@ -301,12 +280,12 @@ def ko_data(model, queryset, all_fields=None, safe=False):
     return model_data_string
 
 
-def ko(model, queryset, all_fields=None):
-    if not all_fields:
-        all_fields = _get_fields(model)
+def ko(model, queryset, knockout_model=None):
+    if not knockout_model:
+        knockout_model = KnockoutModel(model)
 
-    ko_data_string = ko_data(model, queryset, all_fields)
-    ko_view_model_string = ko_view_model(model, all_fields)
+    ko_data_string = ko_data(model, queryset, knockout_model)
+    ko_view_model_string = ko_view_model(model, knockout_model)
     ko_bindings_string = ko_bindings(model)
 
     ko_string = ko_data_string + ko_view_model_string + ko_bindings_string
