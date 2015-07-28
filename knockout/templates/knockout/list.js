8366bb23fd3fe766d0ccb29e2483ed6f6bda53e5
Solanar <and_555@hotmail.com>
2015-07-28 01:15:59 -0600

    add create (for pre-rendered lib) and destroy model helper functions, cleaned up sort functions, add afterAdd function (for pre-rendered lib) updates __prefix__ in template and total form count, add sorted flag to prevent afterAdd firing on sort, add clickChecked function for fancy

--- knockout/templates/knockout/list.js
+++ knockout/templates/knockout/list.js
@@ -15,18 +15,94 @@
         self.{{ model_args }}.push(new {{ model_name }}());
     };
 
+    self.create{{ model_name }} = function() {
+        return new {{ model_name }}();
+    };
+
     self.remove{{ model_name }} = function(data) {
         self.{{ model_args }}.remove(data);
     };
 
+    self.destroy{{ model_name }} = function(data) {
+        self.{{ model_args }}.destroy(data);
+    };
+
+    var sorted = false;
+
     self.sort{{ model_name }}sAsc = function() {
-        self.{{ model_args }}(self.{{ model_args }}().sort(function(a, b) {
-            return a.{{ comparator }}()>b.{{ comparator }}()?-1:a.{{ comparator }}()<b.{{ comparator }}()?1:0;
-         }));
+        self.{{ model_args }}.sort(function(a, b) {
+            var a_comparator = a.{{ comparator }}();
+            var b_comparator = b.{{ comparator }}();
+            if (!a_comparator) { a_comparator = undefined; }
+            if (!b_comparator) { b_comparator = undefined; }
+            var result = a_comparator>b_comparator?-1:a_comparator<b_comparator?1:0;
+
+            sorted = true;
+
+            return result;
+        });
     };
 
     self.sort{{ model_name }}sDesc = function() {
-        self.{{ model_args }}(self.{{ model_args }}().sort(function(a, b) {
-            return a.{{ comparator }}()<b.{{ comparator }}()?-1:a.{{ comparator }}()>b.{{ comparator }}()?1:0;
-        }));
+        self.{{ model_args }}.sort(function(a, b) {
+            var a_comparator = a.{{ comparator }}();
+            var b_comparator = b.{{ comparator }}();
+            if (!a_comparator) { a_comparator = undefined; }
+            if (!b_comparator) { b_comparator = undefined; }
+            var result = a_comparator<b_comparator?-1:a_comparator>b_comparator?1:0;
+
+            sorted = true;
+
+            return result;
+        });
+    };
+
+    self.afterAdd{{ model_name }} = function(elems) {
+        for (var i = 0; i < elems.length; ++i) {
+            if (elems[i].status == 'added') {
+                var template = document.getElementById(empty_form_prefix);
+                template.removeAttribute('id');
+
+                var prefix = elems[i].value.form_prefix();
+                if (!prefix) {
+                    prefix = empty_form_prefix.replace(/__prefix__/,
+                                                       total_form_count.value);
+                }
+
+                var children = template.children;
+                self._update_prefix(children, prefix);
+
+                if (!sorted) {
+                    total_form_count.value = parseInt(total_form_count.value) + 1;
+                }
+            }
+        }
+
+        sorted = false;
+    };
+
+    self._update_prefix = function(children, prefix) {
+        for (var i = 0; i < children.length; ++i) {
+            var child = children[i];
+            var attributes = child.attributes;
+            for (var k = 0; k < attributes.length; ++k) {
+                var attribute = attributes[k];
+                var re = new RegExp(empty_form_prefix);
+                var value = attribute.value.replace(re, prefix);
+                attribute.value = value;
+            }
+
+            self._update_prefix(child.children, prefix);
+        }
+    }
+
+    self.clickChecked = function(data, event) {
+        if (event.currentTarget.checked) {
+            event.currentTarget.parentElement.style.opacity = 0.5;
+        }
+        else {
+            event.currentTarget.parentElement.style.opacity = "";
+        }
+
+        return true;
     };
