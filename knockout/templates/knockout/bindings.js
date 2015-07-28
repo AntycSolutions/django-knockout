4bdfd7599d970a459a93f79ff763640997b1bb74
Solanar <and_555@hotmail.com>
2015-07-28 01:16:12 -0600

    add optional kwarg ignore_data

--- knockout/templates/knockout/bindings.js
+++ knockout/templates/knockout/bindings.js
@@ -1,3 +1,4 @@
 
-ko.applyBindings(new {{ view_model_string }}({{ model_data_string }}),
+var data = (!{{ ignore_data|yesno:"true,false" }} && typeof {{ model_data_string }} !== 'undefined') ? {{ model_data_string }} : null;
+ko.applyBindings(new {{ view_model_string }}(data),
                  document.getElementById("{% if element_id %}{{ element_id }}{% else %}{{ view_model_string|lower }}{% endif %}"));
