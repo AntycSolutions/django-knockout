3d0f83bddc5481e6d0f594bcb2f574d80071fb19
Solanar <and_555@hotmail.com>
2016-02-14 11:17:20 -0700

    refactor
    - rename KnockoutForm -> KnockoutModelForm
    - explicit super (python 2 support)
    - knockout_field_names to rename field name bindings
    - knockout_exclude to exclude fields from being binded

--- demo_example/app/forms.py
+++ demo_example/app/forms.py
@@ -6,7 +6,7 @@ from knockout import forms as knockout_forms
 from app import models
 
 
-class PersonForm(knockout_forms.KnockoutForm):
+class PersonForm(knockout_forms.KnockoutModelForm):
 
     class Meta:
         model = models.Person
--- knockout/forms.py
+++ knockout/forms.py
@@ -1,19 +1,46 @@
 from django import forms
 
 
-class KnockoutForm(forms.ModelForm):
+class KnockoutModelForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(KnockoutModelForm, self).__init__(*args, **kwargs)
+
+        if hasattr(self, 'knockout_field_names'):
+            knockout_field_names = self.knockout_field_names
+        else:
+            knockout_field_names = {}
+
+        if hasattr(self, 'knockout_exclude'):
+            knockout_exclude = self.knockout_exclude
+        else:
+            knockout_exclude = []
+
+        if hasattr(self, 'knockout_init'):
+            knockout_init = self.knockout_init
+        else:
+            knockout_init = False
 
         for field in self:
-            attr = "init, "
+            if field.name in knockout_exclude:
+                continue
 
-            if field.field.widget.__class__.__name__ in ['CheckboxInput']:
+            attr = ""
+
+            if knockout_init:
+                attr = "init, "
+
+            widget_name = field.field.widget.__class__.__name__
+            if widget_name in ['CheckboxInput']:
                 attr += "click: $root.clickChecked, checked: "
+            elif widget_name in ['ClearableFileInput']:
+                pass
             else:
                 attr += "value: "
 
-            attr += field.name
+            if field.name in knockout_field_names:
+                attr += knockout_field_names[field.name]
+            else:
+                attr += field.name
 
             field.field.widget.attrs['data-bind'] = attr
