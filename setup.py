6bcdfb6c77c78acffef1945f81cdb47d86bcb3cc
Solanar <and_555@hotmail.com>
2016-10-27 13:17:33 -0600

    update package info
    - LICENSE
    - update MANIFEST
    - refactor setup.py, use correct License classifier, add more classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
-from setuptools import setup
+from os import path
+import setuptools
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(path.join(path.dirname(__file__), 'README.md')).read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(path.normpath(path.join(path.abspath(__file__), os.pardir)))
 
-setup(
+setuptools.setup(
     name='django-knockout',
     version='0.4.0',  # major.minor[.patch][sub]
     packages=['knockout'],
@@ -19,15 +20,18 @@ setup(
     author='Rich Jones, Andrew Charles',
     author_email='andrew.charles@antyc.ca',
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: JavaScript',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
