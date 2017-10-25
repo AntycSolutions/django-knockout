b94c4c8c5bd673790207a4dd7297c41ac184eef4
Solanar <and_555@hotmail.com>
2017-10-25 15:13:52 -0600

    change license, update versions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
-from os import path
 import setuptools
+from os import path
+
 
 README = open(path.join(path.dirname(__file__), 'README.md')).read()
 
@@ -9,28 +10,31 @@ os.chdir(path.normpath(path.join(path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name='django-knockout',
-    version='0.4.0',  # major.minor[.patch][sub]
+    version='0.4.0',  # major.minor[.patch]
     packages=['knockout'],
     install_requires=['django'],
     include_package_data=True,
-    license='MIT License',
+    license='Apache',
     description='Generate knockout.js View Models from Django Models.',
     long_description=README,
     url='https://github.com/AntycSolutions/django-knockout',
-    author='Rich Jones, Andrew Charles',
+    author='Rich Jones, Andrew B. Charles',
     author_email='andrew.charles@antyc.ca',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
